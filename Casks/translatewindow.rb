628307cf1815983ce59b7d2382f4af6e07cb03a0
BearChao <bearchao@BearChaodeMacBook-Air.local>
2026-08-03 12:43:04 +0800

    chore: update TranslateWindow cask to 1.4.7

--- Casks/translatewindow.rb
+++ Casks/translatewindow.rb
@@ -1,6 +1,6 @@
 cask "translatewindow" do
-  version "1.4.6"
-  sha256 "7e1cd341c39556b47a6273b9924f77738ce36111561218d6698c96892d6ef548"
+  version "1.4.7"
+  sha256 "ebf6997f1393aa22ab75160a1cdb34e97a26e2936fb42d692be291989a7cc4ab"
 
   url "https://release.bearchao.com/translate-window/updates/TranslateWindow-#{version}.dmg"
   name "TranslateWindow"
