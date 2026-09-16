74c29b15ee248f33a5821824b1134882d70eacf3
BearChao <bearchao@BearChaodeMacBook-Air.local>
2026-09-17 01:02:21 +0800

    Update TranslateWindow to 1.6.5 for Apple Silicon and Intel

--- Casks/translatewindow.rb
+++ Casks/translatewindow.rb
@@ -1,8 +1,17 @@
 cask "translatewindow" do
-  version "1.6.0"
-  sha256 "ae94006ebf1008e8f31555fa555bfe0ba7c7c1116a1272554c9158cfba8908ca"
+  version "1.6.5"
 
-  url "https://release.bearchao.com/translate-window/updates/TranslateWindow-#{version}.dmg"
+  on_arm do
+    sha256 "45ccafca4d86e82328b9325d87b04ba4dbc3226c8b86f35f9933af9deb7574b3"
+
+    url "https://release.bearchao.com/translate-window/updates/TranslateWindow-#{version}.dmg"
+  end
+  on_intel do
+    sha256 "f1b9717176203496b8770ed45fa8a11e8dfced314cb330d419c8b373815371ae"
+
+    url "https://release.bearchao.com/translate-window/updates/intel/TranslateWindow-Intel-#{version}.dmg"
+  end
+
   name "TranslateWindow"
   name "小窗译"
   desc "AI translation for selected text, screenshots, and live windows"
@@ -14,7 +23,6 @@ cask "translatewindow" do
   end
 
   auto_updates true
-  depends_on arch: :arm64
   depends_on macos: :sequoia
 
   app "TranslateWindow.app"
